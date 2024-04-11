89be46e22a157afca3031d63dcb88fd2beae9d58
qasim12343 <qasem.yousifi20@gmail.com>
2024-04-12 00:35:50 +0330

    test calculation method for global and local BC

--- Blockchain.py
+++ Blockchain.py
@@ -24,9 +24,10 @@ class BlockChain:
     def create_genesis_block(self):
         return Block(0, "0", None, None)
 
-    def __init__(self):
+    def __init__(self, typ):
         self.head = self.create_genesis_block()
         self.tail = self.head
+        self.typ = typ
 
     def add_block(self, data):
         new_block = Block(data, self.tail.get_hash(), self.tail, None)
@@ -34,16 +35,20 @@ class BlockChain:
         self.tail = new_block
 
     def calculation(self):
-        current = self.head
+        current = self.head.next_block
         result = 0
         while current:
-            result = current.get_data()+result
+            if self.typ == "L":
+                result = current.get_data()+result
+            else:
+                result = current.data.calculation() + result
             current = current.next_block
         return result
 
     def assert_correct(self):
         current = self.head
         while current.next_block:
+
             next_block = current.next_block
             if current.calc_hash() != next_block.previous_hash:
                 return False
@@ -52,13 +57,24 @@ class BlockChain:
 
 
 def main():
-    blockchain = BlockChain()
-    blockchain.add_block(1)
-    blockchain.add_block(-1)
-    blockchain.add_block(-1)
-    blockchain.add_block(5)
+    blockchain1 = BlockChain("L")
+    blockchain2 = BlockChain("L")
+    blockchain3 = BlockChain("G")
+    blockchain1.add_block(1)
+    blockchain1.add_block(-1)
+    blockchain1.add_block(-1)
+    blockchain1.add_block(5)
+    blockchain2.add_block(1)
+    blockchain2.add_block(-1)
+    blockchain2.add_block(-1)
+    blockchain2.add_block(6)
 
-    print(blockchain.calculation())
+    blockchain3.add_block(blockchain1)
+    blockchain3.add_block(blockchain2)
+
+    print(blockchain1.calculation())
+    print(blockchain2.calculation())
+    print(blockchain3.calculation())
     # print(blockchain.assert_correct())
 
     # # corrupt a block
